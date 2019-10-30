855c06a940e833e94f7f66821590d30cc5e5714a
iamcharlesvalentine <charlesvalentine@gmail.com>
2019-10-29 19:49:03 -0500

    Create SwiperApp.js

---
+++ SwiperApp.js
@@ -0,0 +1,59 @@
+import {
+  View,
+  Text
+} from 'react-native';
+import { Swiper } from 'react-native-deck-swiper';
+import React, { Component } from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  View, Image, Text
+} from 'react-native';
+
+
+render() {
+  <View style={styles.container}>
+    <Swiper
+      cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+      renderCard={(card) => {
+        return (
+          <View style={styles.card}>
+            <Text style={styles.text}>{card}</Text>
+          </View>
+        )
+      }}
+      onSwiped={(cardIndex) => { console.log(cardIndex) }}
+      onSwipedAll={() => { console.log('onSwipedAll') }}
+      cardIndex={0}
+      backgroundColor={'#4FD0E9'}
+      stackSize={3}>
+      <Button
+        onPress={() => { console.log('oulala') }}
+        title="Press me">
+        You can press me
+            </Button>
+    </Swiper>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
+});
